05f8504ab88ae8f95cbc11cfecc1abcf722d418d
ybh1760 <ybh1760@gmail.com>
2022-05-30 23:05:03 +0900

    feat: :sparkles: add ItemModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { ItemModule } from '@item/item.module';
 import { MysqlDatabaseProviderModule } from '@providers/database/mysql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MysqlDatabaseProviderModule],
+  imports: [MysqlDatabaseProviderModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
