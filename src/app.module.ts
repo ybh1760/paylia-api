5137e7d14f694006b7cf3bed95860aedefcfdccd
ybh1760 <ybh1760@gmail.com>
2022-06-03 22:53:10 +0900

    chore : add AuthModule, UserModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '@auth/auth.module';
 import { ItemModule } from '@item/item.module';
+import { UserModule } from '@user/user.module';
 import { MysqlDatabaseProviderModule } from '@providers/database/mysql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MysqlDatabaseProviderModule, ItemModule],
+  imports: [MysqlDatabaseProviderModule, AuthModule, ItemModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
